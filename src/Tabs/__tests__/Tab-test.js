ba63199db63f4652bf2c6f61a1f1773dddb79601
dromomaniac <evanlhendrix@gmail.com>
2018-03-13 17:27:40 +0100

    Test environment finally set up right, simple rendering tests are passing

--- src/Tabs/__tests__/Tab-test.js
+++ src/Tabs/__tests__/Tab-test.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-import Tab from './Tab';
+import Tab from '../Tab';
 
+describe('Tab', () => {
+	describe('on mount'), () => {
+		test('render a tab',() => {
+			const wrapper = shallow( <Tab value='Hello Tab!' /> );
+			expect(wrapper).toMatchSnapshot();
+		});
 
-test('render a tab',() => {
-	const wrapper = shallow(
-		<Tab> Hello Tab! </Tab>
-	);
-	expect(wrapper).toMatchSnapshot();
+		test('All tabs should have class 'tab' ', () => {
+			const wrapper = mount(<Tab/>);
+			expect(wrapper.props().className).toBe('tab');
+		});
+
+		test(`click on tab should add className = 'is-active'`, () => {
+			const wrapper = mount
+		});
+	});
 });
-
-test('render a tab with default ')
+		// test('render a tab with is-active class', () => {
+// 	const wrapper = mount(<Tab />);
+//
+// })
 
 
 
